9445fdc68117a6015efab55930c416e604cbf0c4
Krish <krish.gediya@webiots.com>
2024-11-18 17:54:50 +0530

    push

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import "@testing-library/jest-dom";
 
-it("renders learn react link", () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
