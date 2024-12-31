2df283c721aa9af17b69861131932724d6e47266
Krish <krish.gediya@webiots.com>
2024-12-31 17:14:41 +0530

    push

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 import "./i18n";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { unstable_batchedUpdates } from "react-dom";
+
+interface Props {
+  children: React.ReactNode;
+}
+unstable_batchedUpdates(() => {
+  console.error = () => {};
+});
+
+class ErrorBoundary extends Component<Props> {
+  componentDidCatch(error: { message: string | string[] }) {
+    if (error.message.includes("ToastContainer")) {
+      return;
+    }
+  }
+  render() {
+    return this.props.children;
+  }
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
+  <ErrorBoundary>
     <App />
-  </React.StrictMode>
+  </ErrorBoundary>
 );
 
 // If you want to start measuring performance in your app, pass a function
